18e80745b08484bb19c52041793c4307fbce477d
Nikolas Melgarejo <nikoclops@gmail.com>
2018-10-15 13:27:40 -0400

    Adds Semi Working Register To Site Logic

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
+const User = require('../models/')
 const Folder = require('../models/folder');
+
 const { folders } = require('../db/seed/folders');
 const Note = require('../models/note');
 const { notes } = require ('../db/seed/notes');
@@ -12,7 +14,9 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => mongoose.connection.db.dropDatabase())
   .then(() => {
     return Promise.all([
+      User.insertMany(users),
       Note.insertMany(notes),
+      
       Folder.insertMany(folders),
       Folder.createIndexes(),
     ]);
